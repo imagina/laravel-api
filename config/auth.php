f742303daa5f68e580597a2c2285fc59f14c1d83
William <williamverde10@gmail.com>
2025-06-03 18:02:29 -0400

    IMPORTANT: configuration after install passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
@@ -62,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env('AUTH_MODEL', Modules\Iuser\Models\User::class),
         ],
 
         // 'users' => [
@@ -93,7 +97,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'iuser__password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
