4301c9c87d8bcc18d21486ad8b91bc403677a98d
William <williamverde10@gmail.com>
2025-06-20 17:35:01 -0400

    Update: settings

--- Modules/Imedia/config/settings.php
+++ Modules/Imedia/config/settings.php
@@ -1,35 +1,31 @@
 <?php
 
 return [
-    //    'setting_name' => [
-    //        'default' => 'Default Value',
-    //        'isTranslatable' => false,
-    //    ]
     'allowedImageTypes' => [
         'onlySuperAdmin' => true,
-        'name' => 'media::allowedImageTypes',
-        'value' => config('asgard.media.config.allowedImageTypes'),
+        'name' => 'imedia::allowedImageTypes',
+        'default' => config('imedia.allowedImageTypes'),
         'type' => 'select',
         'columns' => 'col-12 col-md-6',
         'props' => [
             'useInput' => true,
             'useChips' => true,
             'multiple' => true,
-            'hint' => 'media::settings.hint.allowedTypes',
+            'hint' => 'imedia::settings.hint.allowedTypes',
             'hideDropdownIcon' => true,
             'newValueMode' => 'add-unique',
-            'label' => 'media::settings.label.allowedImageTypes',
+            'label' => 'imedia::settings.label.allowedImageTypes',
         ],
     ],
     'defaultImageSize' => [
         'onlySuperAdmin' => true,
-        'name' => 'media::defaultImageSize',
-        'value' => config('asgard.media.config.defaultImageSize'),
+        'name' => 'imedia::defaultImageSize',
+        'default' => config('imedia.defaultImageSize'),
         'label' => 'Default Image Size',
         'type' => 'json',
         'columns' => 'col-12',
         'props' => [
-            'label' => 'media::settings.label.defaultImageSize',
+            'label' => 'imedia::settings.label.defaultImageSize',
             'type' => 'textarea',
         ],
 
