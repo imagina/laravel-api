12c297fc4d088e1a9225ecee58e8a108934e1560
William <williamverde10@gmail.com>
2025-06-24 15:29:48 -0400

    Update: title in permissions to Imedia, Isetting

--- Modules/Imedia/config/permissions.php
+++ Modules/Imedia/config/permissions.php
@@ -3,43 +3,43 @@
 return [
     'imedia.files' => [
         'manage' => [
-            'title' =>   'imedia::files.manage resource',
+            'title' =>   'imedia::files.manage.title',
             'description' => 'imedia::files.manage.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index' => [
-            'title' =>   'imedia::files.index resource',
+            'title' =>   'imedia::files.index.title',
             'description' => 'imedia::files.index.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index-all' => [
-            'title' =>   'imedia::files.index-all resource',
+            'title' =>   'imedia::files.index-all.title',
             'description' => 'imedia::files.index-all.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'create' => [
-            'title' =>   'imedia::files.create resource',
+            'title' =>   'imedia::files.create.title',
             'description' => 'imedia::files.create.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'edit' => [
-            'title' =>   'imedia::files.edit resource',
+            'title' =>   'imedia::files.edit.title',
             'description' => 'imedia::files.edit.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'destroy' => [
-            'title' =>   'imedia::files.destroy resource',
+            'title' =>   'imedia::files.destroy.title',
             'description' => 'imedia::files.destroy.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'restore' => [
-            'title' =>   'imedia::files.restore resource',
+            'title' =>   'imedia::files.restore.title',
             'description' => 'imedia::files.restore.description',
             'onlyFor' => [],
             'defaultAccess' => []
@@ -47,37 +47,37 @@ return [
     ],
     'imedia.zones' => [
         'manage' => [
-            'title' =>   'imedia::zones.manage resource',
+            'title' =>   'imedia::zones.manage.title',
             'description' => 'imedia::zones.manage.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index' => [
-            'title' =>   'imedia::zones.index resource',
+            'title' =>   'imedia::zones.index.title',
             'description' => 'imedia::zones.index.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'create' => [
-            'title' =>   'imedia::zones.create resource',
+            'title' =>   'imedia::zones.create.title',
             'description' => 'imedia::zones.create.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'edit' => [
-            'title' =>   'imedia::zones.edit resource',
+            'title' =>   'imedia::zones.edit.title',
             'description' => 'imedia::zones.edit.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'destroy' => [
-            'title' =>   'imedia::zones.destroy resource',
+            'title' =>   'imedia::zones.destroy.title',
             'description' => 'imedia::zones.destroy.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'restore' => [
-            'title' =>   'imedia::zones.restore resource',
+            'title' =>   'imedia::zones.restore.title',
             'description' => 'imedia::zones.restore.description',
             'onlyFor' => [],
             'defaultAccess' => []
--- Modules/Isetting/config/permissions.php
+++ Modules/Isetting/config/permissions.php
@@ -3,37 +3,37 @@
 return [
     'isetting.settings' => [
         'manage' => [
-            'title' =>   'isetting::settings.list resource',
+            'title' =>   'isetting::settings.list.title',
             'description' => 'isetting::settings.manage.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'index' => [
-            'title' =>   'isetting::settings.list resource',
+            'title' =>   'isetting::settings.list.title',
             'description' => 'isetting::settings.index.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'create' => [
-            'title' =>   'isetting::settings.create resource',
+            'title' =>   'isetting::settings.create.title',
             'description' => 'isetting::settings.create.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'edit' => [
-            'title' =>   'isetting::settings.edit resource',
+            'title' =>   'isetting::settings.edit.title',
             'description' => 'isetting::settings.edit.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'destroy' => [
-            'title' =>   'isetting::settings.destroy resource',
+            'title' =>   'isetting::settings.destroy.title',
             'description' => 'isetting::settings.destroy.description',
             'onlyFor' => [],
             'defaultAccess' => []
         ],
         'restore' => [
-            'title' =>   'isetting::settings.restore resource',
+            'title' =>   'isetting::settings.restore.title',
             'description' => 'isetting::settings.restore.description',
             'onlyFor' => [],
             'defaultAccess' => []
