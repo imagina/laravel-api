0e3bc86c006ba321f7d42ab625b53189aaf8b90c
Cristian Marin <marin101998@gmail.com>
2025-07-10 12:10:17 -0500

    + add | 10-07-25 fix path migrations in service provider

--- Modules/Ifillable/app/Providers/IfillableServiceProvider.php
+++ Modules/Ifillable/app/Providers/IfillableServiceProvider.php
@@ -39,7 +39,7 @@ class IfillableServiceProvider extends ServiceProvider
         $this->registerTranslations();
         $this->registerConfig();
         $this->registerViews();
-        $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+        $this->loadMigrationsFrom(module_path($this->name, 'database/Migrations'));
     }
 
     /**
