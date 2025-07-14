effd21fa9b908a4732970f0c53bbd566ea2f1ace
Cristian Marin <marin101998@gmail.com>
2025-07-14 11:52:01 -0500

    + add | 14-07-25 test notifications in leads entity

--- Modules/Iform/app/Models/Lead.php
+++ Modules/Iform/app/Models/Lead.php
@@ -19,7 +19,12 @@ class Lead extends CoreModel
     //Instance external/internal events to dispatch with extraData
     public array $dispatchesEventsWithBindings = [
         //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
-        'created' => [],
+        'created' => [
+            [
+                'path' => 'Modules\Inotification\Events\SendNotification',
+                'extraData' => ['event' => 'created']
+            ]
+        ],
         'creating' => [],
         'updated' => [],
         'updating' => [],
@@ -44,6 +49,27 @@ class Lead extends CoreModel
     public function assignedTo()
     {
         return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    /**
+     * Notification Params
+     */
+    public function getNotificableParams()
+    {
+        //Process to get Email (Example: From entity, or settings, etc)
+        $email = 'emailto@example.com';
+
+        return [
+            'created' => [
+                "email" => $email,
+                "title" =>  itrans("iform::leads.email.created.title"),
+                "content" => "iform::emails.textForm",
+                "extraParams" => [
+                    "lead" => $this,
+                    "form" => $this->form
+                ],
+            ],
+        ];
     }
 
 }
