8fa3c8120e688d7bb16e59bd4b4841034e0e23a0
William <williamverde10@gmail.com>
2025-07-14 09:45:12 -0400

    added: validation not return login route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Evita redirección automática al login
+        $middleware->redirectGuestsTo(fn() => null);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        //Validation exception Login (Not return to blade)
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            return response()->json([
+                "errorCode" => 401,
+                "message" => "Unauthorized"
+            ], 401);
+        });
     })->create();
