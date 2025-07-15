30cb55941b29c81a91d71ddc71e37ae313adeb53
William <williamverde10@gmail.com>
2025-07-15 12:02:48 -0400

    Update: added confit to test layout emails

--- Modules/Inotification/routes/web.php
+++ Modules/Inotification/routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
-use Modules\Inotification\Services\NotificationDispatcherService;
+use Modules\Inotification\Services\NotificationService;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+
 
 
 $router->group(['prefix' => '/inotification/v1'], function (Router $router) {
@@ -12,20 +12,8 @@ $router->group(['prefix' => '/inotification/v1'], function (Router $router) {
     //Show Render Example Email
     if (app()->environment('local')) {
         Route::get('/preview-email', function (Request $request) {
-            $to = $request->query('email');
 
-            $data = [
-                'title' => itrans("inotification::notification.email.default.title"),
-                'message' => itrans("inotification::notification.email.default.message")
-            ];
-
-            if ($to) {
-                //Validation Email
-                if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
-                    return response()->json(['error' => 'Invalid Email'], Response::HTTP_UNPROCESSABLE_ENTITY);
-                }
-                app(NotificationDispatcherService::class)->execute(['email' => $to]);
-            }
+            $data = app(NotificationService::class)->getDataExample($request);
 
             return view('inotification::emails.contents.default', compact('data'));
         });
