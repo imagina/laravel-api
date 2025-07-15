c3e355d8a8c867411acc57ec17705dd101fc4a91
William <williamverde10@gmail.com>
2025-07-15 12:38:08 -0400

    Delete: methods not used | Iuser

--- Modules/Iuser/app/Services/AuthService.php
+++ Modules/Iuser/app/Services/AuthService.php
@@ -56,25 +56,4 @@ class AuthService
             'expiresAt' => $issuedAt->copy()->addSeconds($expiresIn)->getTimestampMs()
         ];
     }
-
-    /**
-     *
-     */
-    public function logUserIn($user)
-    {
-        Auth::login($user); //OJO: esto nose si sea necesario
-        session()->regenerate();
-    }
-
-    /**
-     *
-     */
-    public function logUserOut()
-    {
-        //Method Laravel\Passport\Guards\TokenGuard::logout does not exist.
-        //Auth::logout();
-
-        session()->invalidate();
-        session()->regenerateToken();
-    }
 }
