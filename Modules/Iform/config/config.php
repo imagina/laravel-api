654afee373ec48ec8582780d0f92c1fb101238cf
William <williamverde10@gmail.com>
2025-07-15 12:06:53 -0400

    Added: config to lead - test email

--- Modules/Iform/config/config.php
+++ Modules/Iform/config/config.php
@@ -2,4 +2,14 @@
 
 return [
 
+    'leadTestEmail' => [
+        "title" =>  itrans("iform::lead.email.created.title"),
+        "message" => 'El mensaje',
+        "content" => "iform::emails.lead",
+        "extraParams" => [
+            'lead' => fn() => Modules\Iform\Models\Lead::find(1),
+            'form' => fn() => Modules\Iform\Models\Form::with('fields')->find(1),
+        ],
+    ]
+
 ];
