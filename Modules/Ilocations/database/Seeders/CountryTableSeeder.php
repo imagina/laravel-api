a5e4519713e5f28b19d3c7f7d3408c4bfe0e94e6
Cristian Marin <marin101998@gmail.com>
2025-08-04 18:13:28 -0500

    + add Provinces Cities and countries in module ilocations

---
+++ Modules/Ilocations/database/Seeders/CountryTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Ilocations\Database\Seeders;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+use Modules\Ilocations\Models\Country;
+
+class CountryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Model::unguard();
+
+        $path = base_path('/Modules/Ilocations/app/Assets/js/countries.json');
+        $countries = json_decode(file_get_contents($path), true);
+
+        foreach ($countries as $key => $country) {
+            $currentCountry = Country::where('iso_3', $country['iso_3'])->first();
+
+            if (! isset($currentCountry->id)) {
+                Country::create($country);
+            }
+        }
+    }
+}
