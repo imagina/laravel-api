96c143baeb714408cf6fb457d47b5019f8e9d800
William <williamverde10@gmail.com>
2025-08-06 12:21:13 -0400

    Update: new format settings | Imedia

--- Modules/Imedia/config/settings.php
+++ Modules/Imedia/config/settings.php
@@ -5,30 +5,33 @@ return [
         'onlySuperAdmin' => true,
         'name' => 'imedia::allowedImageTypes',
         'default' => config('imedia.allowedImageTypes'),
-        'type' => 'select',
-        'columns' => 'col-12 col-md-6',
-        'props' => [
-            'useInput' => true,
-            'useChips' => true,
-            'multiple' => true,
-            'hint' => 'imedia::settings.hint.allowedTypes',
-            'hideDropdownIcon' => true,
-            'newValueMode' => 'add-unique',
-            'label' => 'imedia::settings.label.allowedImageTypes',
-        ],
+        'dynamicField' => [
+            'type' => 'select',
+            'columns' => 'col-12 col-md-6',
+            'props' => [
+                'useInput' => true,
+                'useChips' => true,
+                'multiple' => true,
+                'hint' => 'imedia::settings.hint.allowedTypes',
+                'hideDropdownIcon' => true,
+                'newValueMode' => 'add-unique',
+                'label' => 'imedia::settings.label.allowedImageTypes',
+            ],
+        ]
     ],
     'defaultImageSize' => [
         'onlySuperAdmin' => true,
         'name' => 'imedia::defaultImageSize',
         'default' => config('imedia.defaultImageSize'),
-        'label' => 'Default Image Size',
-        'type' => 'json',
-        'columns' => 'col-12',
-        'props' => [
-            'label' => 'imedia::settings.label.defaultImageSize',
-            'type' => 'textarea',
-        ],
-
+        'dynamicField' => [
+            'label' => 'Default Image Size',
+            'type' => 'json',
+            'columns' => 'col-12',
+            'props' => [
+                'label' => 'imedia::settings.label.defaultImageSize',
+                'type' => 'textarea',
+            ],
+        ]
     ],
 
 ];
